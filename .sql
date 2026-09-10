fc4513db592b81d19f5904909ffe6cd18247c1e9
Rifki Abrori <rifkiabrori57@gmail.com>
2026-09-10 16:21:25 +0700

    update code

---
+++ .sql
@@ -0,0 +1,15 @@
+USE AbsensiDB;
+
+SELECT 
+    a.Id,
+    k.NIK,
+    k.Nama,
+    a.Tanggal,
+    a.JamMasuk,
+    a.JamKeluar,
+    a.Status,
+    a.Keterangan,
+    a.CreatedAt
+FROM Absensi a
+JOIN Karyawan k ON a.KaryawanId = k.Id
+ORDER BY a.Tanggal DESC, a.JamMasuk DESC;
